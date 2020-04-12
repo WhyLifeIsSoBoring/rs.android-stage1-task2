9ffe4a6275bae6b5c2e7652eddd3261c0c502b9e
Maksim Stankevich <maksimka.stankevich.2001@mail.ru>
2020-04-12 17:21:19 +0300

    feat: add subtask3 solution

--- src/main/kotlin/subtask3/Abbreviation.kt
+++ src/main/kotlin/subtask3/Abbreviation.kt
@@ -1,9 +1,25 @@
 package subtask3
 
+import java.lang.StringBuilder
+
 class Abbreviation {
 
     // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+        var test = StringBuilder(a.toUpperCase())
+        var count = 0
+
+        while(count <= b.length) {
+            if(test[count] != b[count]) {
+                test.deleteCharAt(count)
+                count--
+            }
+            count++
+        }
+        if (count == b.length)
+            return "YES"
+        else {
+            return "NO"
+        }
     }
 }
