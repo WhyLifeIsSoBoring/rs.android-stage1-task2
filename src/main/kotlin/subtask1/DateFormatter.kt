d7b2b09c17f3348e73ded1a69a21a18d74eed613
Maksim Stankevich <maksimka.stankevich.2001@mail.ru>
2020-04-12 16:04:36 +0300

    feat: add subtask1 solution

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,9 +1,25 @@
 package subtask1
 
+import java.time.DateTimeException
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeParseException
+import java.util.*
+
 class DateFormatter {
 
     // TODO: Complete the following function
     fun toTextDay(day: String, month: String, year: String): String {
-        throw NotImplementedError("Not implemented")
+       return  try {
+           var form = DateTimeFormatter.ofPattern("dd MMMM, EEEE", Locale("Ru"))
+           LocalDate
+               .of(year.toInt(), month.toInt(), day.toInt())
+               .format(form)
+
+       } catch (e: DateTimeParseException) {
+           "Такого дня не существует"
+       } catch (e: DateTimeException) {
+           "Такого дня не существует"
+       }
     }
 }
