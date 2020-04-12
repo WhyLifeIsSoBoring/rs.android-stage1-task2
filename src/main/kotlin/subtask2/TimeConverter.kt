59a6753824669ddab33d7fcb262d69fa4b3056be
Maksim Stankevich <maksimka.stankevich.2001@mail.ru>
2020-04-12 16:53:49 +0300

    feat: add subtask2 solution

--- src/main/kotlin/subtask2/TimeConverter.kt
+++ src/main/kotlin/subtask2/TimeConverter.kt
@@ -4,6 +4,36 @@ class TimeConverter {
 
     // TODO: Complete the following function
     fun toTextFormat(hour: String, minute: String): String {
-        throw NotImplementedError("Not implemented")
+        val arrayOfNumbers = arrayOf(
+            "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+            "eleven", "twelve", "thirteen", "fourteen", "quarter", "sixteen", "seventeen", "eighteen", "nineteen",
+            "twenty", "twenty one", " twenty two", "twenty three", "twenty four", "twenty five", "twenty six",
+            "twenty seven", "twenty eight", "twenty nine", "half"
+        )
+
+        var hour = hour.toInt()
+        var minute = minute.toInt()
+        var res = ""
+
+        if (minute == 0) {
+            res = arrayOfNumbers[hour] + " o' clock"
+        } else if (minute in 1..30) {
+            if (minute == 1) {
+                res = arrayOfNumbers[0] + " minute to " + arrayOfNumbers[hour]
+            } else if (minute == 15) {
+                res = arrayOfNumbers[minute] + " past " + arrayOfNumbers[hour]
+            } else {
+                res = arrayOfNumbers[minute] + " minutes past " + arrayOfNumbers[hour]
+            }
+        } else if (minute in 31..59) {
+            if (minute == 59) {
+                res = arrayOfNumbers[0] + " minute to " + arrayOfNumbers[hour]
+            } else if (minute == 45) {
+                res = arrayOfNumbers[60 - minute] + " to " + arrayOfNumbers[hour]
+            } else {
+                res = arrayOfNumbers[60 - minute] + " minutes to " + arrayOfNumbers[hour]
+            }
+        }
+        return res
     }
 }
